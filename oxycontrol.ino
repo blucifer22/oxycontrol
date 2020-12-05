0ea7d756b86a41ea852d4180e6a7345552edea66
Marc Chmielewski <msc68@duke.edu>
2020-12-05 16:16:56 -0500

    setup complete

--- oxycontrol.ino
+++ oxycontrol.ino
@@ -1,9 +1,35 @@
+#include <LiquidCrystal.h>
+#include <Servo.h>
+
 // This runs at power-on or when the board is reset
 void setup() {
-
+    
+    // PIN CONFIGS!!!
+    // Move servo to initial position
+    // Turn the light off
+    // Initialize pill number to 14
+    // Start time at zero
+    // Initialize count up to zero
+    // Display some start-up message on the LCD
 }
 
 // This runs continuously while the board is powered
 void loop() {
-    
+    // While pill number > 0
+    // if time is !0
+    // Update time
+    // Display time
+    // else
+    // Light button
+    // Dispense now on LCD
+    // Start count-up time
+    // if button is pressed && count-up is less than 24
+    // Reset count up
+    // Increment servo
+    // Decrement pill count
+    // Reset count down
+    // Turn off light
+    // elif count-up >= 24 BREAK
+
+    // LOCKOUT and display take to pharm
 }
